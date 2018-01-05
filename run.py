c53353905ef991e182287d802dd213577dd81603
X1r0z <admin@exp10it.cn>
2018-01-05 18:18:47 +0800

    change homepage

--- run.py
+++ run.py
@@ -34,15 +34,5 @@ def index():
     else:
         return render_template('index.html')
 
-@app.route('/about')
-def about():
-
-    return render_template('about.html')
-
-@app.route('/contact')
-def contact():
-
-    return render_template('contact.html')
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
